b20d42cdd08b682306cd4dd5f77d620857ad1668
Mark A. Chandler, II <mark.allen.chandler@gmail.com>
2016-03-19 07:19:57 -0400

    Create Voting component

---
+++ src/components/Voting.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Voting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  render() {
+    return (
+      <div className="voting">
+        {this.getPair().map(entry =>
+          <button key={entry}
+                  onClick={() => this.props.vote(entry)}>
+            <h1>{entry}</h1>
+          </button>
+         )}
+      </div>
+    );
+  }
+}
+
+export default Voting;
