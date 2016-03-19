9daec9a105e260ada657c7986d8004d8b3a52d2e
Mark A. Chandler, II <mark.allen.chandler@gmail.com>
2016-03-19 18:30:13 +0000

    Create Vote component

---
+++ src/components/Vote.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Vote extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
+  hasVotedFor(entry) {
+    return this.prop.hasVoted === entry;
+  }
+
+  render() {
+    return (
+      <div className="voting">
+        {this.getPair().map(entry =>
+          <button key={entry}
+                  disabled={this.isDisabled()}
+                  onClick={() => this.props.vote(entry)}>
+            <h1>{entry}</h1>
+            {this.hasVotedFor(entry) ?
+              <div className="label">Voted</div> :
+              null}
+          </button>
+        )}
+      </div>
+    );
+  }
+}
