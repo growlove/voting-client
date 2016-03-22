4bc1c7df9d37a27fbeb57df7e5fa769859eae64d
Mark A. Chandler, II <mark.allen.chandler@gmail.com>
2016-03-22 14:16:25 +0000

    Remove default data from App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
 
-const pair = List.of('Trainspotting', '28 Days Later');
-const tally = Map({'Trainspotting': 5, '28 Days Later': 4})
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
+export default class App extends React.Component {
   render() {
-    return React.cloneElement(this.props.children, {
-      pair,
-      tally
-    });
+    return this.props.children;
   }
 }
-export default App;
